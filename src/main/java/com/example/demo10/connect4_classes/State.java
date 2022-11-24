c1c05e9d2eb927a1f085c979ba34ab48cf83f6a8
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2022-11-24 08:45:12 +0200

    basic state class for connect4 game added

---
+++ src/main/java/com/example/demo10/connect4_classes/State.java
@@ -0,0 +1,16 @@
+package com.example.demo10.connect4_classes;
+
+public class State {
+    public static final short RED = -1;
+    public static final short EMPTY = 0;
+    public static final short YELLOW = 1;
+
+    private int [][] disksArray = new int [7][6];
+    private int [] columnsPointers = new int [6];
+
+    private int lastMoveX = -1;
+    private int lastMoveY = -1;
+
+
+
+}
