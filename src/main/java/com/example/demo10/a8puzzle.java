8534d2f122ab7e3bc61d23ce8c25ca05a331e96f
mohamadw22 <mohamadw22@hotmail.com>
2022-11-26 04:04:09 +0200

    front end bug-needs fixing

--- src/main/java/com/example/demo10/a8puzzle.java
+++ src/main/java/com/example/demo10/a8puzzle.java
@@ -1,4 +1,86 @@
 package com.example.demo10;
 
-public class a8puzzle {
+public class a8puzzle implements Comparable<a8puzzle>{
+    int[][] grid;
+    int cost=Integer.MAX_VALUE;
+    a8puzzle parent;
+
+    public a8puzzle[] makechildren(a8puzzle curr){
+        int posx = 0,posy = 0;
+
+
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                if (curr.grid[i][j]==9){
+                    posy=i;
+                    posx=j;
+                }
+            }
+        }
+
+        int counterab=0;
+        if (posx+1!=3)counterab++;
+        if (posx-1!=-1)counterab++;
+        if (posy+1!=3)counterab++;
+        if (posy-1!=-1)counterab++;
+        a8puzzle [] children=new a8puzzle[counterab];
+
+
+            a8puzzle child=new a8puzzle(curr.grid);
+        int count=0;
+        if (posx+1!=3){
+            int v;
+
+            v=child.grid[posx][posy];
+            child.grid[posx][posy]=child.grid[posx+1][posy];
+            child.grid[posx+1][posy]=v;
+            children[count++]=child;
+            child.parent=curr;
+            child=new a8puzzle(curr.grid);
+        }
+        if (posx-1!=-1){
+            int v;
+            v=child.grid[posx][posy];
+            child.grid[posx][posy]=child.grid[posx-1][posy];
+            child.grid[posx-1][posy]=v;
+            children[count++]=child;
+            child.parent=curr;
+            child=new a8puzzle(curr.grid);
+        }
+        if (posy+1!=3){
+            int v;
+            v=child.grid[posx][posy];
+            child.grid[posx][posy]=child.grid[posx][posy+1];
+            child.grid[posx][posy+1]=v;
+            children[count++]=child;
+            child.parent=curr;
+            child=new a8puzzle(curr.grid);
+        }
+        if (posy-1!=-1){
+            int v;
+            v=child.grid[posx][posy];
+            child.grid[posx][posy]=child.grid[posx][posy-1];
+            child.grid[posx][posy-1]=v;
+            children[count++]=child;
+            child.parent=curr;
+
+        }
+
+
+
+
+        return children;
+    }
+    public a8puzzle(int[][] grid) {
+        this.grid=new int[3][3];
+        for (int i=0;i<3;i++)
+            for (int j=0;j<3;j++)
+                this.grid[i][j]=grid[i][j];
+    }
+
+    @Override
+    public int compareTo(a8puzzle o) {
+        if(o.cost<cost)return -1;
+        else return 1;
+    }
 }
